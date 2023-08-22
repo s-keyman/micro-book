6657d95f0a1fff39c05431f8104b5fed24eb266c
Keyma <s.keyman0@gmail.com>
2023-08-23 01:15:18 +0800

    使用官方 middleware 解决跨域问题

--- microBook/main.go
+++ microBook/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	"micro-book/microBook/internal/web"
 )
 
 func main() {
 	server := gin.Default()
-	u := &web.UserHandler{}
+	server.Use(cors.New(cors.Config{
+		//允许的单个路由（建议用 AllowOriginFunc ）
+		//AllowOrigins:     []string{"https://foo.com"},
+		//不填 AllowMethods 代表允许所有方法（ post put get 等，详见文档）
+		AllowMethods: []string{"PUT", "POST", "GET", "OPTIONS"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		//是否允许带 cookie 之类的东西
+		//ExposeHeaders:    []string{"x-jwt-token"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				//开发环境
+				return true
+			}
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Second,
+	}))
+	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
-	server.Run("localhost:8080")
+	server.Run(":8080")
 }
